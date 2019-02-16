c16966bc40f3e946f45492db3853cd37a6200fe8
Juan Luis Almazo <jluisalmazo@hotmail.com>
2019-02-15 23:02:52 -0600

    First commmit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JuanLuisAlmazo
+Examen práctico para Juan Luis
+Las instrucciones se encuentra en el siguiente drive:
+
+https://docs.google.com/document/d/1SZXNhOmerEsBUCzNgHmRTnyEHweH9uYg2QktAmxu0AE/edit?usp=sharing
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
+
+// Init App
+const app = express();
+
+// Load View Engine (PUG)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Sass config
+app.use(
+    express.static(__dirname + '/views/'),
+    sassMiddleware({
+        src: __dirname + '/views', 
+        dest: __dirname + '/views',
+        debug: true
+    })
+);
+
+// Home Route
+app.get('/', (req, res) => {    
+    res.render('index', {
+        title: 'Prueba',
+    });
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+});
